dd5b619792b958e7bec78f8d8f2cd355a99d05f5
Ioana Vasiliu <ioana.vasiliu@boots.co.uk>
2022-03-04 17:02:44 +0200

    final

--- addFeedback.js
+++ addFeedback.js
@@ -8,6 +8,8 @@ const addFeedback = async (req, res) => {
 
         const feedbacks = db.collection("feedbacks");
 
+        const badFeedbacks = db.collection("badFeedbacks");
+
         const { type, isSolved, rating, suggestion } = req.body;
 
         const newFeedback = {
@@ -19,30 +21,33 @@ const addFeedback = async (req, res) => {
 
         await feedbacks.insertOne(newFeedback);
 
-        const avgx = await feedbacks.aggregate([
-            {
-                $match: {
-                    type: newFeedback.type
+        const avgx = await feedbacks
+            .aggregate([
+                {
+                    $match: {
+                        type: newFeedback.type,
+                    },
                 },
-            },
-            {
-                $group: {
-                    _id: "avg",
-                    avg: {
-                        "$avg": "$rating"
-                    }
-                }
-            }
-        ]).toArray();
+                {
+                    $group: {
+                        _id: "avg",
+                        avg: {
+                            $avg: "$rating",
+                        },
+                    },
+                },
+            ])
+            .toArray();
 
-
-
-        const avg = avgx[0].avg.toFixed(2)
+        const avg = avgx[0].avg.toFixed(2);
 
         if (avg >= 3) {
             console.log("All good!");
         } else {
-            console.log("Average rating is lower than 3. Slack alert!");
+            await badFeedbacks.insertOne({
+                type: newFeedback.type,
+                averageRating: avg,
+            });
         }
     } catch (err) {
         console.log("Error while adding feedback: ", err);
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const addFeedback = require('./addFeedback')
 
 const app = express();
+app.use(cors());
 
 const port = 3000;
 
